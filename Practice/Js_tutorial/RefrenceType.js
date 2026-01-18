6b9561407f7f59df9d1c6686c66a334cd89d8e46
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-19 00:24:22 +0530

    Refrence type with its Example fo Object and Arrays

---
+++ Practice/Js_tutorial/RefrenceType.js
@@ -0,0 +1,23 @@
+// refreence type -> Object, arrays and function
+
+// Object:
+let friend={
+    name:"amol",
+    age:25,
+    smoking:true
+}
+console.log(friend);
+console.log(friend.smoking);
+console.log(typeof(friend));
+console.log(friend['smoking']);
+
+//Arrays:
+
+let arr=["vaibhav",24,"BCS",false,73.59]
+console.log(arr);
+console.log(typeof(arr));
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
