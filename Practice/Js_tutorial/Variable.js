2bb5370e75f2a6399a461dcd782073800939db2f
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-18 23:20:40 +0530

    JS premetive variables

---
+++ Practice/Js_tutorial/Variable.js
@@ -0,0 +1,26 @@
+// Premetive values
+// Number, String, Boolean, undefined, null, symbole
+// Dynamic programing 
+
+let a =12;
+console.log(a);
+console.log(typeof(a));
+
+let b="Vaibhav";
+console.log(b);
+console.log(typeof(b));
+
+let c=true;
+console.log(c);
+console.log(typeof(c));
+
+let d
+console.log(d);
+console.log(typeof(d));
+
+let e=null;
+console.log(e);
+console.log(typeof(e));
+
+e="bhinge"; // JS the dynamic programing language tha tmeans we can chagnge the type of the alriablewith its value 
+console.log(typeof(e));
