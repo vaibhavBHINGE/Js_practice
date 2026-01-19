0acd7457180b3f598f705f9a1dc8e764b8353910
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-19 23:08:50 +0530

    addted funcation and its type with examples

--- Practice/Js_tutorial/RefrenceType.js
+++ Practice/Js_tutorial/RefrenceType.js
@@ -1,23 +1,52 @@
 // refreence type -> Object, arrays and function
 
 // Object:
-let friend={
-    name:"amol",
-    age:25,
-    smoking:true
+let friend = {
+    name: "amol",
+    age: 25,
+    smoking: true
 }
 console.log(friend);
 console.log(friend.smoking);
-console.log(typeof(friend));
+console.log(typeof (friend));
 console.log(friend['smoking']);
 
 //Arrays:
 
-let arr=["vaibhav",24,"BCS",false,73.59]
+let arr = ["vaibhav", 24, "BCS", false, 73.59]
 console.log(arr);
-console.log(typeof(arr));
+console.log(typeof (arr));
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
 console.log(arr[3]);
 console.log(arr[4]);
+
+//----- Functions 
+
+function fri(name) {
+    console.log("My friend name is: " + name);
+}
+fri("Ganesh");
+fri("Harsh");
+fri(1);
+
+// Type of funcations
+
+// Funcation decleartion 
+function fun(a, b) {
+    return a - b;
+}
+console.log("substraction f=of give number is: " + fun(9, 2));
+
+// Funcation Expresion
+
+let add = function (a, b, c) {
+    return a + b + c;
+}
+console.log("Addation fo give three numbers is: " + add(1, 2, 3))
+
+// arrow funcation
+
+const mul = (a, b) => a * b;
+console.log(mul(2, 2));
