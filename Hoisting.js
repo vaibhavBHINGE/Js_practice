190378c0dffd402234923d4a9f392a7f50d0c9d5
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-20 19:44:52 +0530

    deleted hoisting file

--- Hoisting.js
+++
@@ -1,9 +0,0 @@
-// Hoiesting 
-// Hoiesting in JS happnes because of js exucation context. While execution js code it happenes in two phase
-// 1. Memory allocation
-// 2. code is execuated line by line 
-
-console.log("Value in X: "+X);
-
-var X=12;
-console.log(this.a);
