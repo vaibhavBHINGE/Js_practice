eec5772774c0517e9a5cf5ae7c14ba16ebc71c3f
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-20 19:48:13 +0530

    new hoisting file added with its exapmple and explaintation

---
+++ Practice/Js_tutorial/Hoisting.js
@@ -0,0 +1,9 @@
+// Hoiesting 
+// Hoiesting in JS happnes because of js exucation context. While execution js code it happenes in two phase
+// 1. Memory allocation
+// 2. code is execuated line by line 
+
+console.log("Value in X: "+X);
+
+var X=12;
+console.log(this.a);
