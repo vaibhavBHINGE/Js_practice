041e81f20edf35e5a9b65c0a95c7a0864f1e7ab0
Vaibhav Bhinge <vbhinge2711@gmail.com>
2026-01-21 21:32:57 +0530

    new function concept with its example and theory

--- Practice/Js_tutorial/RefrenceType.js
+++ Practice/Js_tutorial/RefrenceType.js
@@ -50,3 +50,13 @@ console.log("Addation fo give three numbers is: " + add(1, 2, 3))
 
 const mul = (a, b) => a * b;
 console.log(mul(2, 2));
+
+
+//Higher order function
+
+// A common funcation is returning or passing function as an arguments that is called as hiher order function.
+
+function Hi(fun, a,b){
+return (fun,a,b);
+}
+Hi(mul,2,3);
